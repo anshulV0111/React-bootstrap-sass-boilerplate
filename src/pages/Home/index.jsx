0b688199d92d7bcf2cde3169e664c71d749bf84a
Anshul Verma <anshulv0111@gmail.com>
2022-08-21 21:32:41 +0530

    react bootstrap sass boilerplate

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import Banner from '../../components/Banner'
+import Footer from '../../components/Footer'
+import Form from '../../components/Form'
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Form />
+      <Footer />
+    </>
+  )
+}
+export default Home
